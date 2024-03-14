e88f333591fb2ea61c5703bfe8d2b5e77859be7d
saeh sharifi <bia2znd@gmail.com>
2024-03-14 11:16:39 +0330

    n to br

--- app/Http/Controllers/AupostController.php
+++ app/Http/Controllers/AupostController.php
@@ -98,7 +98,7 @@ class AupostController extends Controller
             if($target=AuPostTargets::where('slug',$post->target)->where('is_active',1)->where('updated_at','<',Carbon::now()->subDay()->toDateTimeString())->first()){
                 if($target->kind==1){
                     $post->nonhtml=strip_tags($post->content);
-                    $post->nonhtml=str_replace('\n','
+                    $post->nonhtml=str_replace("\n",'
 ',$post->nonhtml);
                     $type = pathinfo($post->img, PATHINFO_EXTENSION);
                     $data = @file_get_contents($post->img);
